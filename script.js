f2cba02ea6eb43222efd96ea9ca62679f1e20ae4
ezekielsung <61392425+ezekielsung@users.noreply.github.com>
2020-11-11 15:19:16 -0800

    added modal

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const open = document.getElement('Done');
-const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('complete')
+var modal = document.getElementById("id1");
 
-open.addEventListener('click', ()=> {
-  modal_container.classList('show');
-});
 
-close.addEventListener('click', ()=> {
-  modal_container.classList.remove('show');
-})
+// Get the button that opens the modal
+var btn = document.getElementById("done");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+
+  }
+}
