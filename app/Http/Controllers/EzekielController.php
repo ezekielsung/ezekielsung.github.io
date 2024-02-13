c2752f6d0f267bcc0dbf3a4113ea958ec3cb197c
ezekiels96 <ezekielsung96@gmail.com>
2024-02-12 19:37:47 -0800

    a

---
+++ app/Http/Controllers/EzekielController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EzekielController extends Controller
+{
+    public function joanneIndex()
+    {
+        return view('021424.index'); // Assuming you have a view file at resources/views/joanne/index.blade.php
+    }
+}
