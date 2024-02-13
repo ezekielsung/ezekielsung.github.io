53080d7403cc4f47770f4f29d4440d7540b4343e
ezekiels96 <ezekielsung96@gmail.com>
2024-02-12 19:22:30 -0800

    a

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-var modal = document.getElementById("id1");
+const img = document.getElementById('bouncingImage');
+let imgPos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+let velocity = { x: 5, y: 5 };
 
+img.addEventListener('click', function() {
+  console.log('clicked');
+    img.style.display = 'none'; // Hide the bouncing image
+    document.body.style.backgroundImage = 'url("dam.jpg")'; // Update background image
+    document.body.style.opacity = '0.5';
+    
+    document.getElementById('riddleContainer').style.display = 'block'; // Show riddle container
+});
 
-// Get the button that opens the modal
-var btn = document.getElementById("done");
+function updatePosition() {
+    if (imgPos.x + img.clientWidth >= window.innerWidth || imgPos.x <= 0) {
+        velocity.x = -velocity.x;
+    }
+    if (imgPos.y + img.clientHeight >= window.innerHeight || imgPos.y <= 0) {
+        velocity.y = -velocity.y;
+    }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    imgPos.x += velocity.x;
+    imgPos.y += velocity.y;
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+    img.style.left = imgPos.x + 'px';
+    img.style.top = imgPos.y + 'px';
 
+    requestAnimationFrame(updatePosition);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-
-  }
-}
+updatePosition();
